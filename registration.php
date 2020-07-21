d2e513f0a496846e0d444b031865f47f56788774
Andrew Stowell <andrew@digitaladapt.com>
2020-07-21 15:06:59 -0400

    Added missing registration.php file.

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'DigitalAdapt_RedirectMissing',
+    __DIR__
+);
